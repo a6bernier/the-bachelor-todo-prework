f725fa9e6b8f6c2d84b43c3c5545fed13723b663
Andrew Bernier <a6bernier@gmail.com>
2018-09-12 20:17:39 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   get_first_name_of_season_winner.each do |season|
     get_first_name_of_season_winner.each do |name|
-      name.split("") 
+      name.first.split("") 
 end
 
 def get_contestant_name(data, occupation)
