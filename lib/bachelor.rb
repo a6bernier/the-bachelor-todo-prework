02dc278dc1f4d5156b0825938f7826b795717690
Andrew Bernier <a6bernier@gmail.com>
2018-09-12 20:23:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,10 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |info|
     if info["status"] == "Winner"
-      return info["name"].split.first  
-    end    
-  end   
-end  
+      return info["name"].split.first
+    end
+  end
+end
 
 def get_contestant_name(data, occupation)
   # code here
